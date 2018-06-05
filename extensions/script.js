1e9496b5f374a13edd9d25e718a6e0e8dce97e40
kobayashi takuya <kobataku0629@gmail.com>
2018-06-05 23:30:00 +0900

    省略可能箇所を省略

--- extensions/script.js
+++ extensions/script.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMSubtreeModified", function (event) {
+document.addEventListener("DOMSubtreeModified", () => {
   const messages = Array.from(document.getElementsByClassName("timelineMessage__message"));
   eyeToUnti(messages);
 });
 
-const eyeToUnti = (nodes) => {
+const eyeToUnti = nodes => {
   Array.from(nodes).forEach(node => {
     if (node.nodeType === Node.TEXT_NODE) {
       const text = node.textContent;
@@ -11,7 +11,7 @@ const eyeToUnti = (nodes) => {
         node.textContent = text.replace("👁️", "💩");
       }
     } else {
-      eyeToUnti(node.childNodes)
+      eyeToUnti(node.childNodes);
     }
   });
-}
+};
